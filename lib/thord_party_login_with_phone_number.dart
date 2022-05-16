ecbf473b9259183812c15d618660dbf842b5d331
Natnael Adane <knatnaelaadane@gmail.com>
2022-05-16 10:13:53 +0300

    bug fix and added some comments

--- lib/thord_party_login_with_phone_number.dart
+++ lib/thord_party_login_with_phone_number.dart
@@ -3,6 +3,7 @@ import 'package:third_party_login/common/utils.dart';
 
 ///ThirdPartyLoginWithPhoneNumber
 abstract class ThirdPartyLoginWithPhoneNumber {
+  ///verify phone number
   Future<void> verifyPhoneNumber(
       {required String phoneNumber, int? timeOut, int? resendToken}) async {
     try {
@@ -19,14 +20,22 @@ abstract class ThirdPartyLoginWithPhoneNumber {
     }
   }
 
+  ///on verification complete this method is called by firebase auth
   verificationCompleted(PhoneAuthCredential phoneAuthCredential);
 
+  ///on verification failed this method is called by firebase auth
   verificationFailed(FirebaseAuthException authException);
 
+  ///on code send this method is called by firebase auth
   codeSent(String verificationId, int? forceResendingToken);
 
+  ///on code auto retrieval timed out  this method is called by firebase auth
   codeAutoRetrievalTimeout(String verificationId);
 
+  ///This method will start phone number verification
+  ///pass [phoneNumber] which is required to verify this is required
+  ///pass [timeOut] this is time out for code auto retrieval default is 30sec
+  ///pass [resendToken] when you want to resend the code
   Future<void> init(
       {required String phoneNumber, int? timeOut, int? resendToken}) async {
     await verifyPhoneNumber(
