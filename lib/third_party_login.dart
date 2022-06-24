a0d850a5b979809fe89e880d4c4615453f5dabc4
Natnael Adane <knatnaelaadane@gmail.com>
2022-06-24 09:22:06 +0300

    missing import

--- lib/third_party_login.dart
+++ lib/third_party_login.dart
@@ -2,6 +2,7 @@ library third_party_login;
 
 export 'package:firebase_auth/firebase_auth.dart';
 import 'package:third_party_login/third_party_login.dart';
+import 'package:third_party_login/third_party_login_with_apple_signin.dart';
 import 'package:third_party_login/third_party_login_with_facebook_signin.dart';
 import 'package:third_party_login/third_party_login_with_google_signin.dart';
 
